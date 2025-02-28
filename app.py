0b95c486b102fa1ba45a30de4f92bab944f5fb1e
IDGS-805-21000017 <98120436+IDGS-805-21000017@users.noreply.github.com>
2025-02-27 18:51:16 -0600

    Layout

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash, g
+from flask_wtf.csrf import CSRFProtect
+from config import DevelopmentConfig
+
+from models import db, Alumnos
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+csrf = CSRFProtect()
 
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html")
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	return render_template("index.html"), 404
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	csrf.init_app(app)
+	db.init_app(app)
+	with app.app_context():
+		db.create_all()
+	app.run()
+
