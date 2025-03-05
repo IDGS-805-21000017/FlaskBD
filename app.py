b5ded509f5aa907167a3d3b095ba22c91654f267
IDGS-805-21000017 <98120436+IDGS-805-21000017@users.noreply.github.com>
2025-03-04 18:55:02 -0600

    Agregar

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, g
 from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
+import forms as f
 
 from models import db, Alumnos
 
@@ -9,12 +10,44 @@ app.config.from_object(DevelopmentConfig)
 csrf = CSRFProtect()
 
 @app.errorhandler(404)
-def page_not_found(e):
-	return render_template("404.html")
+def page_not_found(error):
+    return render_template("404.html"), 404
+
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html"), 404
+	create_form = f.UserForm2(request.form)
+	alumnos = Alumnos.query.all() # SELECT * FROM alumnos
+
+	return render_template("index.html", form=create_form, alumnos=alumnos), 404
+
+@app.route("/detalles", methods=['GET','POST'])
+def alumnoDetalle():
+	create_form = f.UserForm2(request.form)
+	if request.method == 'GET':
+		id=request.args.get('id')
+		alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+		print(alum1)
+		nom = alum1.nombre
+		apaterno = alum1.apaterno
+		email = alum1.email
+
+	return render_template("detalles.html", form=create_form, nombre=nom, apellido=apaterno, email=email)
+
+@app.route("/crear", methods=['GET', 'POST'])
+def crear():
+	create_form = f.UserForm2(request.form)
+	if request.method == "POST":
+		alum = Alumnos(
+			nombre=create_form.nombre.data,
+			apaterno=create_form.apaterno.data,
+			email = create_form.email.data
+		)
+		db.session.add(alum)
+		db.session.commit()
+		return redirect(url_for("index"))
+	return render_template("crear.html", form=create_form)
+	
 
 if __name__ == '__main__':
 	csrf.init_app(app)
